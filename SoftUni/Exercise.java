77c09f1adf8e0206f1bb28ec5c8286ad3f81444d
V-Gulev <velizargulev@abv.bg>
2025-01-08 17:25:50 +0200

    Stacks and Queues - Exercise

--- SoftUni/Exercise.java
+++ SoftUni/Exercise.java
@@ -1,10 +1,17 @@
+package SoftUni;
+
 import java.util.ArrayDeque;
 import java.util.Scanner;
 
-public class Main {
+public class Exercise {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         ArrayDeque<Integer> stack = new ArrayDeque<>();
-
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+        stack.push(4);
+        stack.push(5);
+        System.out.println(stack);
     }
 }
